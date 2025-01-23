0500a433e5187382bac6cc07d321819ffe9fd078
Vinicius Gasparello da Silva <gasparellovinicius@gmail.com>
2025-01-22 22:08:33 -0300

    feat: wallet and transactions entiti and controller

---
+++ CarteiraDigital.API/Controllers/TransactionController.cs
@@ -0,0 +1,24 @@
+﻿using CarteiraDigital.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarteiraDigital.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        public TransactionController()
+        {
+            
+        }
+
+        [HttpGet("GetTransactionsByUser/{id}")]
+        public async Task<ActionResult<IEnumerable<Transaction>>> TransactionsByUser(string userId)
+        {
+
+
+            return Ok();
+        }
+    }
+}
---
+++ CarteiraDigital.API/Controllers/WalletController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarteiraDigital.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalletController : ControllerBase
+    {
+    }
+}
